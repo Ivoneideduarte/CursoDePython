581c4b46af939f792d906f3c811cfc84bb5423b6
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2020-06-30 16:23:42 -0300

    Corrigi um erro de comentário

--- CursoDePython3/Mundo 1 - Fundamentos/03 - Usando módulos do Python/Desafio 27/desafio27.py
+++ CursoDePython3/Mundo 1 - Fundamentos/03 - Usando módulos do Python/Desafio 27/desafio27.py
@@ -9,6 +9,6 @@ n = str(input('Digite seu nome completo: ')).strip()
 nome = n.split() #Este comando split() serve para fatiar uma palavra em pedaços
 #print(nome) #Mostra a lista de caracteres
 print('Muito prazer em te conhecer!')
-print('Seu primeiro nome é {}'.format(nome[0])) #Mosttra o primeiro item da lista
+print('Seu primeiro nome é {}'.format(nome[0])) #Mostra o primeiro item da lista
 print('Seu último nome é {}'.format(nome[len(nome)-1])) #Com o comando len() posso saber quantas posições foram salvas na variável nome
 
