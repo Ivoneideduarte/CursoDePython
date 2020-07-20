baf90ffb079a4230d040850a4bacbeb19dd23f72
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2020-07-20 15:53:43 -0300

    Nova atualização

--- CursoDePython3/Mundo 1 - Fundamentos/01 - Primeiros passos com o Python/Desafio 01/desafio01.py
+++ CursoDePython3/Mundo 1 - Fundamentos/01 - Primeiros passos com o Python/Desafio 01/desafio01.py
@@ -1,6 +1,5 @@
 #Desafio 01
 #Faça um programa que leia o nome de uma pessoa e mostre na tela uma mensagem de boas-vindas.
 
-#print('É um prazer te conhecer, ' + nome)
-nome = input('Digite seu nome: ')
-print('É um prazer te conhecer, {}'.format(nome))
+nome = input('Qual o seu nome? ')
+print('Olá, ' + nome + '.' + ' Prazer em te conhecer!')
--- CursoDePython3/Mundo 1 - Fundamentos/01 - Primeiros passos com o Python/Desafio 02/desafio02.py
+++ CursoDePython3/Mundo 1 - Fundamentos/01 - Primeiros passos com o Python/Desafio 02/desafio02.py
@@ -1,9 +1,14 @@
 #Desafio 02
 #Faça um programa que leia o dia, o mês e o ano de nascimento de uma pessoae mostre uma mensagem com a data formatada.
 
-nome = input('Informe o seu nome: ')
-print('Informe seus dados de nascimento, abaixo:')
 dia = input('Dia: ')
 mes = input('Mês: ')
 ano = input('Ano: ')
-print('Você nasceu no dia ', dia + '/' + mes + '/' + ano + '.', 'correto, ', nome + '?')
+print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + '.' + ' Correto?')
+
+#Desafio 03
+#Faça um programa que leia dois números e tente mostrar a soma entre eles. Esse programa irá dar um erro, pois necessita de mais alguns recursos para funcionar.
+
+n1 = input('Digite um número: ')
+n2 = input('Digite outro número: ')
+print('A soma é '+ n1+n2)
